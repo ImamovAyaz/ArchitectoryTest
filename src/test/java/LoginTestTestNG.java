11aadc32db37367f53df8b65d42855ab58072a8d
Imamov Ayaz <ayaz.imamov.99@mail.ru>
2022-12-23 06:58:45 +0300

    Add DataProvider for LoginTestTestNG

--- src/test/java/LoginTestTestNG.java
+++ src/test/java/LoginTestTestNG.java
@@ -1,23 +1,40 @@
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 
 public class LoginTestTestNG extends BaseTest {
-    @Test
-    public void loginTest() {
+    @DataProvider(name = "credentials") // Создаем метод-провайдер
+    public Object[][] getDataFromDataprovider() {
+        return new Object[][]{
+                {"login1", "password1"},
+                {"login2", "password2"},
+                {"login3", "password3"}
+        };
+    }
+    @Test(dataProvider = "credentials")
+    public void loginTest(String login, String password) {
         // TODO: Add reading from default input file
         // TODO***: Add xml serialization \ deSerialization for LoginData
         // 3A Rule
 
-        try(FileReader file = new FileReader(".idea/LoginPassword")) {
+        LoginData ld = new LoginData(login, password, "+7999", "1234");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(ld);
+        System.out.println("Логин/Пароль:" + LoginData.getLogin() + "/" + ld.getPassword());
+
+        Assert.assertTrue(HomePage.isImportantLabelAvaliable());
+
+       /* try (FileReader file = new FileReader(".idea/LoginPassword")) {
             BufferedReader reader = new BufferedReader(file);
             String line = reader.readLine();
             System.out.println(line);
 
-            while (line != null){
+            while (line != null) {
                 String[] array = line.split(";");
                 //LoginData ld = new LoginData(array[0], array[1]);
                 LoginData ld = new LoginData("login", "password", "+7999", "1234");
@@ -29,10 +46,9 @@ public class LoginTestTestNG extends BaseTest {
                 line = reader.readLine();
             }
 
-        }
-        catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
-        }
+        }*/
 
     }
 }
